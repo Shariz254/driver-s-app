a192215debad9231d076d3fca75636accc3cd225
Shariz254 <sakinaseif01@gmail.com>
2022-06-20 10:31:49 +0300

    Loaded Map

--- lib/tabPages/home_tab.dart
+++ lib/tabPages/home_tab.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
+
+import 'package:driver_app/widgets/divider.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeTabPage extends StatefulWidget {
   const HomeTabPage({Key? key}) : super(key: key);
@@ -7,10 +12,453 @@ class HomeTabPage extends StatefulWidget {
   State<HomeTabPage> createState() => _HomeTabPageState();
 }
 
-class _HomeTabPageState extends State<HomeTabPage> {
+class _HomeTabPageState extends State<HomeTabPage>
+{
+
+  final Completer<GoogleMapController> _controllerGoogleMap = Completer();
+  GoogleMapController? newGoogleMapController;
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  GlobalKey<ScaffoldState> scaffoldKey = new GlobalKey<ScaffoldState>();
+
+  /**Geo locator positions**/
+  late Position currentPosition;
+  var geolocator = Geolocator();
+
+  double bottomPaddingOfMap = 0;
+
+  void locatePosition() async
+  {
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    currentPosition = position;
+    
+    //getting latitude and longitude
+    LatLng latLngPosition = LatLng(position.latitude, position.longitude);
+
+    //location moves when you move
+    CameraPosition cameraPosition = new CameraPosition(target: latLngPosition, zoom: 14);
+    newGoogleMapController?.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+  }
+  /**Geo locator positions**/
+
+
+  /**for black theme**/
+  blackThemeGoogleMap()
+  {
+    /**for black theme google map**/
+    newGoogleMapController!.setMapStyle('''
+                    [
+                      {
+                        "elementType": "geometry",
+                        "stylers": [
+                          {
+                            "color": "#242f3e"
+                          }
+                        ]
+                      },
+                      {
+                        "elementType": "labels.text.fill",
+                        "stylers": [
+                          {
+                            "color": "#746855"
+                          }
+                        ]
+                      },
+                      {
+                        "elementType": "labels.text.stroke",
+                        "stylers": [
+                          {
+                            "color": "#242f3e"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "administrative.locality",
+                        "elementType": "labels.text.fill",
+                        "stylers": [
+                          {
+                            "color": "#d59563"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "poi",
+                        "elementType": "labels.text.fill",
+                        "stylers": [
+                          {
+                            "color": "#d59563"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "poi.park",
+                        "elementType": "geometry",
+                        "stylers": [
+                          {
+                            "color": "#263c3f"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "poi.park",
+                        "elementType": "labels.text.fill",
+                        "stylers": [
+                          {
+                            "color": "#6b9a76"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "road",
+                        "elementType": "geometry",
+                        "stylers": [
+                          {
+                            "color": "#38414e"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "road",
+                        "elementType": "geometry.stroke",
+                        "stylers": [
+                          {
+                            "color": "#212a37"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "road",
+                        "elementType": "labels.text.fill",
+                        "stylers": [
+                          {
+                            "color": "#9ca5b3"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "road.highway",
+                        "elementType": "geometry",
+                        "stylers": [
+                          {
+                            "color": "#746855"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "road.highway",
+                        "elementType": "geometry.stroke",
+                        "stylers": [
+                          {
+                            "color": "#1f2835"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "road.highway",
+                        "elementType": "labels.text.fill",
+                        "stylers": [
+                          {
+                            "color": "#f3d19c"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "transit",
+                        "elementType": "geometry",
+                        "stylers": [
+                          {
+                            "color": "#2f3948"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "transit.station",
+                        "elementType": "labels.text.fill",
+                        "stylers": [
+                          {
+                            "color": "#d59563"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "water",
+                        "elementType": "geometry",
+                        "stylers": [
+                          {
+                            "color": "#17263c"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "water",
+                        "elementType": "labels.text.fill",
+                        "stylers": [
+                          {
+                            "color": "#515c6d"
+                          }
+                        ]
+                      },
+                      {
+                        "featureType": "water",
+                        "elementType": "labels.text.stroke",
+                        "stylers": [
+                          {
+                            "color": "#17263c"
+                          }
+                        ]
+                      }
+                    ]
+                ''');
+    /**for black theme google map**/
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Home"),
-    );  }
+    return Scaffold(
+      key: scaffoldKey,
+
+      drawer: Container(
+        color: Colors.white,
+        width: 255.0,
+        child: Drawer(
+          child: ListView(
+            children: [
+              //Drawer Header
+              Container(
+                height: 165.0,
+                child: DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                  ),
+                  child: Row(
+                    children: [
+                      Image.asset("images/user.png", height: 65.0, width: 65.0,),
+                      SizedBox(width: 16.0,),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("Profile Name",
+                            style: TextStyle(
+                              fontSize: 16.0,
+                              fontFamily: "Brand-Bold"
+                            ),),
+
+                          SizedBox(height: 6.0,),
+                          
+                          Text("Visit Profile"),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+
+              DividerWidget(),
+
+              SizedBox(height: 12.0,),
+
+              //Drawer Body Buttons
+              ListTile(
+                leading: Icon(Icons.history),
+                title: Text("History", style: TextStyle(fontSize: 15.0),),
+              ),
+
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text("Visit Profile", style: TextStyle(fontSize: 15.0),),
+              ),
+
+              ListTile(
+                leading: Icon(Icons.info),
+                title: Text("About", style: TextStyle(fontSize: 15.0),),
+              ),
+
+            ],
+          ),
+        ),
+      ),
+
+
+      body: Stack(
+        children: [
+          GoogleMap(
+              padding: EdgeInsets.only(bottom: bottomPaddingOfMap),
+              mapType: MapType.normal,
+              myLocationEnabled: true,
+              myLocationButtonEnabled: true,
+              zoomGesturesEnabled: true,
+              zoomControlsEnabled: true,
+              initialCameraPosition: _kGooglePlex,
+
+
+              onMapCreated: (GoogleMapController controller)
+              {
+                _controllerGoogleMap.complete(controller);
+                newGoogleMapController = controller;
+
+                setState(() {
+                  bottomPaddingOfMap = 300.0;
+                });
+
+                locatePosition();
+
+                //blackTheme
+                // blackThemeGoogleMap();
+              },
+          ),
+
+          //Hambeger button for drawer
+          Positioned(
+            top: 45.0,
+            left: 22.0,
+            child: GestureDetector(
+              onTap: ()
+              {
+                scaffoldKey.currentState!.openDrawer();
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(22.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black,
+                      blurRadius: 6.0,
+                      spreadRadius: 0.5,
+                      offset: Offset(0.7, 0.7)
+                    )
+                  ]
+                ),
+
+                child: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: Icon(Icons.menu, color: Colors.black,),
+                  radius: 20.0,
+                ),
+              ),
+            ),
+          ),
+
+
+          Positioned(
+              left: 0.0,
+              right: 0.0,
+              bottom: 0.0,
+              child: Container(
+                height: 300.0,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(topLeft: Radius.circular(18.0), topRight: Radius.circular(18.0)),
+                  boxShadow: [
+                      BoxShadow(
+                        color: Colors.black,
+                        blurRadius: 16.0,
+                        spreadRadius: 0.5,
+                        offset: Offset(0.7, 0.7),
+                      )
+                  ]
+                ),
+
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 18.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 6.0,),
+
+                      Text("Hi there, ",style: TextStyle(fontSize: 12.0),),
+                      Text("Where To .?, ",style: TextStyle(fontSize: 20.0, fontFamily: "Brand-Bold"),),
+
+                      SizedBox(height: 20.0,),
+
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(5.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black54,
+                                blurRadius: 6.0,
+                                spreadRadius: 0.5,
+                                offset: Offset(0.7, 0.7),
+                              )
+                            ]
+                        ),
+                        
+                        child: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: Row(
+                            children: [
+                              Icon(Icons.search, color: Colors.blueAccent,),
+
+                              SizedBox(width: 10.0,),
+
+                              Text("Search Drop Off")
+                            ],
+                          ),
+                        ),
+                      ),
+
+                      SizedBox(height: 24.0,),
+                      Row(
+                        children: [
+                          Icon(Icons.home, color: Colors.grey,),
+                          SizedBox(width: 12.0,),
+
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Add Home"),
+                              SizedBox(height: 4.0,),
+
+                              Text("Your Residential Home Address",
+                              style: TextStyle(color: Colors.black54,
+                              fontSize: 12.0),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+
+
+                      SizedBox(height: 10.0,),
+
+                      DividerWidget(),
+
+                      SizedBox(height: 16.0,),
+
+                      Row(
+                        children: [
+                          Icon(Icons.work, color: Colors.grey,),
+                          SizedBox(width: 12.0,),
+
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Add Work"),
+                              SizedBox(height: 4.0,),
+
+                              Text("Your Office Address",
+                                style: TextStyle(color: Colors.black54,
+                                    fontSize: 12.0),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+
+                    ],
+                  ),
+                ),
+              ),
+          ),
+
+        ],
+      ),
+    );
+  }
 }
